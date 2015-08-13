891191cac41c2edd98599ed41bb1e58ef79c899e
shibayu36 <shibayu36@gmail.com>
2015-08-13 20:24:37 +0900

    Init by Minilla

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Time::At - It's new $module
+
+# SYNOPSIS
+
+    use Test::Time::At;
+
+# DESCRIPTION
+
+Test::Time::At is ...
+
+# LICENSE
+
+Copyright (C) shibayu36.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+shibayu36 <shibayu36@gmail.com>
---
+++ lib/Test/Time/At.pm
@@ -0,0 +1,39 @@
+package Test::Time::At;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Time::At - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Time::At;
+
+=head1 DESCRIPTION
+
+Test::Time::At is ...
+
+=head1 LICENSE
+
+Copyright (C) shibayu36.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+shibayu36 E<lt>shibayu36@gmail.comE<gt>
+
+=cut
+
